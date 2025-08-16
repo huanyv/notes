ad86455a920d65875d3166a8733995d654f59592
huanyv <liylmine@qq.com>
2025-08-16 17:20:41 +0800

    update

--- code/yiyujizhang.sql
+++ code/yiyujizhang.sql
@@ -1,3 +1,10 @@
+
+
+
+
+
+select * from record 
+
 select CASE     
 	when t1."type" =0 then '支出'
 	when t1."type" =1 then '收入'
@@ -24,4 +31,51 @@ left join asset t4 on t4.id=t1.asset_id
 left join asset t5 on t5.id=t1.to_asset_id 
 left join asset t6 on t6.id=t1.from_asset_id  
 order by t1.record_time desc
+
+
+
+select t1.id as 'id',t2.id, t1.name as 'name', t2.name as 'sub_name' from record_category t1
+left join record_category t2 on t2.parent_cat_id =t1.id
+where t1.parent_cat_id = -1
+
+
+select * from record where "type" = 0 order by record_time desc -- 消费
+
+select * from record where "type" = 1 order by record_time desc -- 收入
+
+select * from record where "type" = 2 order by record_time desc -- 转账
+
+select * from record where "type" = 3 order by record_time desc -- 报销
+
+select * from record where "type" = 4 order by record_time desc -- 余额变更
+
+
+
+select distinct asset_id  from record 
+
+select distinct from_asset_id  from record 
+
+
+select distinct record_cat_id  from record 
+
+select distinct sub_type  from record 
+
+select distinct type  from record 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
