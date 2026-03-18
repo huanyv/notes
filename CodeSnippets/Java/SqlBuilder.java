281b03fbc0d563dc987375ad2145750a55e3bfa4
huanyv <liylmine@qq.com>
2026-03-18 07:13:30 +0000

    update CodeSnippets/Java/SqlBuilder.java.

--- CodeSnippets/Java/SqlBuilder.java
+++ CodeSnippets/Java/SqlBuilder.java
@@ -18,7 +18,7 @@ import java.util.function.Consumer;
  *         sb.trim("set", "", "", ",", builder -> {
  *             builder.append(SqlBuilder.hasText(name), " name = ?,", name);
  *             builder.append(SqlBuilder.hasText(age), "  age = ?,  ", age);
- *         }).append("where id = ?", "ididid123");
+ *         }).append("where id = ?", "id");
  *         System.out.println(sb);
  *
  *         sb = new SqlBuilder("insert into t1");
@@ -53,7 +53,7 @@ public class SqlBuilder implements Serializable {
     }
 
     public SqlBuilder append(String str, Object... args) {
-        if (str == null || str.length() == 0) {
+        if (str == null || str.isEmpty()) {
             return this;
         }
         if (this.sql.length() != 0) {
@@ -105,6 +105,71 @@ public class SqlBuilder implements Serializable {
         return this;
     }
 
+    /**
+     * 循环构建SQL
+     * <pre>{@code
+     * Person p1 = new Person("id1", "zhangsan", 18, "111");
+     * Person p2 = new Person("id2", "lisi", null, "222");
+     * Person p3 = new Person("id3", "wangwu", 17, "333");
+     * List<String> names = new ArrayList<>(Arrays.asList("zhang", "li", "wang"));
+     * List<Person> personList = new ArrayList<>(Arrays.asList(p1, p2, p3));
+     *
+     * SqlBuilder sb = new SqlBuilder();
+     * sb = new SqlBuilder("select * from t1");
+     * sb.trim("where", "", "and|or", "", builder -> {
+     * 	builder.append(SqlBuilder.nonNull(p1.getAge()), "age > ?", p1.getAge());
+     * 	builder.foreach("or name in (", ")", ",", names, (nameIn, name, index) -> {
+     * 		nameIn.append(SqlBuilder.hasText(name), "?", name);
+     *    });
+     * });
+     * System.out.println(sb);
+     *
+     * sb = new SqlBuilder("insert into t1");
+     * sb.trim("(", ")", "", ",", builder -> {
+     * 	builder.append("id,");
+     * 	builder.append(" name,");
+     * 	builder.append("  age,  ");
+     * });
+     * sb.append("values");
+     * sb.foreach("", "", ",", personList, (builder, person, index) -> {
+     * 	builder.append("(?,?,?)", person.getId(), person.getName(), person.getAge());
+     * });
+     * System.out.println(sb);
+     * }</pre>
+     *
+     * @param open       开始前缀
+     * @param close      结束后缀
+     * @param separator  分隔符
+     * @param collection 集合
+     * @param func       回调函数
+     * @param <T>        集合元素类型
+     * @return this
+     */
+    public <T> SqlBuilder foreach(String open, String close, String separator, Collection<T> collection, TiConsumer<SqlBuilder, T, Integer> func) {
+        open = open == null ? "" : open;
+        close = close == null ? "" : close;
+        separator = separator == null ? "" : separator;
+        collection = collection == null ? Collections.emptyList() : collection;
+        StringJoiner joiner = new StringJoiner(separator);
+        List<Object> params = new ArrayList<>();
+        int index = 0;
+        for (T item : collection) {
+            SqlBuilder builder = new SqlBuilder();
+            func.accept(builder, item, index);
+            if (!builder.isEmpty()) {
+                joiner.add(builder.getSql());
+                Collections.addAll(params, builder.getArgs());
+            }
+            index++;
+        }
+        if (joiner.length() != 0) {
+            append(open);
+            append(joiner.toString(), params.toArray());
+            append(close);
+        }
+        return this;
+    }
+
     public boolean isEmpty() {
         return this.sql.length() == 0;
     }
@@ -119,7 +184,7 @@ public class SqlBuilder implements Serializable {
 
     @Override
     public String toString() {
-        return "Sql:\t" + getSql() + "\nArgs:\t" + Arrays.toString(getArgs()) + "\n===========================";
+        return "Sql:\t" + getSql() + "\nArgs:\t" + Arrays.toString(getArgs()) + "\n==============================";
     }
 
     public static boolean nonNull(Object o) {
@@ -140,5 +205,8 @@ public class SqlBuilder implements Serializable {
         return false;
     }
 
-
+    @FunctionalInterface
+    public interface TiConsumer<T, K, V> {
+        void accept(T t, K k, V v);
+    }
 }
