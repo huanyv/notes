f15892e66ad3d07665bc5d00f42302203ffa96c7
huanyv <liylmine@qq.com>
2026-03-21 14:45:19 +0800

    update

--- CodeSnippets/Java/SqlBuilder.java
+++ CodeSnippets/Java/SqlBuilder.java
@@ -1,3 +1,4 @@
+package io.github.huanyv.melon.sql.utils;
 
 import java.io.Serializable;
 import java.util.*;
@@ -52,6 +53,13 @@ public class SqlBuilder implements Serializable {
         this.sql.append(str.trim());
     }
 
+    /**
+     * 追加SQL，会自动拼接空格符
+     *
+     * @param str  SQL字符串
+     * @param args SQL参数
+     * @return this
+     */
     public SqlBuilder append(String str, Object... args) {
         if (str == null || str.isEmpty()) {
             return this;
@@ -67,6 +75,14 @@ public class SqlBuilder implements Serializable {
         return this;
     }
 
+    /**
+     * 根据条件追加
+     *
+     * @param condition 条件
+     * @param str       SQL字符串
+     * @param args      SQL参数
+     * @return this
+     */
     public SqlBuilder append(boolean condition, String str, Object... args) {
         if (condition) {
             append(str, args);
@@ -74,6 +90,24 @@ public class SqlBuilder implements Serializable {
         return this;
     }
 
+    public SqlBuilder where(Consumer<SqlBuilder> builder) {
+        return trim("where", "", "and |or |AND |OR ", "", builder);
+    }
+
+    public SqlBuilder set(Consumer<SqlBuilder> builder) {
+        return trim("set", "", "", ",", builder);
+    }
+
+    /**
+     * 自定义拼接SQL字符串，以及前缀、后缀，以及移除多余的前缀或后缀。
+     *
+     * @param prefix          内容不为空，则在内容前面添加指定的字符串（例如 WHERE, SET, ( 等）
+     * @param suffix          内容不为空，则在内容后面添加指定的字符串（例如 ), ; 等）
+     * @param prefixOverrides 内容的开头匹配指定的字符串，则将其移除（常用于移除多余的 AND, OR）
+     * @param suffixOverrides 内容的结尾匹配指定的字符串，则将其移除（常用于移除多余的逗号 ,）
+     * @param builder         回调函数，参数是新的{@link SqlBuilder}
+     * @return this
+     */
     public SqlBuilder trim(String prefix, String suffix, String prefixOverrides, String suffixOverrides, Consumer<SqlBuilder> builder) {
         prefix = prefix == null ? "" : prefix;
         suffix = suffix == null ? "" : suffix;
@@ -105,8 +139,16 @@ public class SqlBuilder implements Serializable {
         return this;
     }
 
+    public <T> SqlBuilder foreach(Collection<T> collection, TiConsumer<SqlBuilder, T, Integer> func) {
+        return foreach("", "", "", collection, func);
+    }
+
+    public <T> SqlBuilder foreach(String separator, Collection<T> collection, TiConsumer<SqlBuilder, T, Integer> func) {
+        return foreach("", "", separator, collection, func);
+    }
+
     /**
-     * 循环构建SQL
+     * 集合遍历的核心元素，主要用于构建IN条件语句或批量插入/更新操作。它允许你遍历一个集合（如 List, Set），并将集合中的每个元素拼接到SQL语句中。
      * <pre>{@code
      * Person p1 = new Person("id1", "zhangsan", 18, "111");
      * Person p2 = new Person("id2", "lisi", null, "222");
